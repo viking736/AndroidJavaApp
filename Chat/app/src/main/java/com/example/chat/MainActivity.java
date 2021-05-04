a4e0168979dbbc621727ad6c980d816894c2f6e4
viking736 <viking25082000@gmail.com>
2021-05-04 18:22:47 +0300

    user check

--- Chat/app/src/main/java/com/example/chat/MainActivity.java
+++ Chat/app/src/main/java/com/example/chat/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SIGN_IN_CODE = 1;
+    private RelativeLayout activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity_main = findViewById(R.id.activity_main);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null){
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        }else {
+            Snackbar.make(activity_main, "Already exist", Snackbar.LENGTH_LONG).show();
+            displayAllMessages();
+        }
+    }
+    private void displayAllMessages(){
+
     }
 }
