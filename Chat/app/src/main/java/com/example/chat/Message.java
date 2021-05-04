6a1a19959a39051f9bcef7f3d4186286ff97b0f2
viking736 <viking25082000@gmail.com>
2021-05-04 18:08:30 +0300

    chat app

---
+++ Chat/app/src/main/java/com/example/chat/Message.java
@@ -0,0 +1,43 @@
+package com.example.chat;
+
+import java.util.Date;
+
+public class Message {
+    public String userName;
+    public String txtMsg;
+    private long msgTime;
+
+    public Message(){ }
+
+    public Message(String userName, String txtMsg){
+        this.userName = userName;
+        this.txtMsg = txtMsg;
+        this.msgTime = new Date().getTime();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTxtMsg() {
+        return txtMsg;
+    }
+
+    public void setTxtMsg(String txtMsg) {
+        this.txtMsg = txtMsg;
+    }
+
+    public long getMsgTime() {
+        return msgTime;
+    }
+
+    public void setMsgTime(long msgTime) {
+        this.msgTime = msgTime;
+    }
+
+
+}
