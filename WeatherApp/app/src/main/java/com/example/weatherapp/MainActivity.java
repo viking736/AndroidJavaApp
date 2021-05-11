ab2026a03edbc3f8680fadf8bff49f30cec424ea
viking736 <viking25082000@gmail.com>
2021-05-11 18:48:44 +0300

    editing weather app

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText user_field;
+    private Button main_button;
+    private TextView result_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user_field = findViewById(R.id.user_field);
+        main_button = findViewById(R.id.main_button);
+        result_info = findViewById(R.id.result_info);
+
+        main_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user_field.getText().toString().trim().isEmpty()){
+                    Toast.makeText(MainActivity.this, R.string.warning, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
