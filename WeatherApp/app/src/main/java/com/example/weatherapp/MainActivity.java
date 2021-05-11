1290aa543e8cf0395879c5af84cf49f7a0d945d8
viking736 <viking25082000@gmail.com>
2021-05-11 19:53:44 +0300

    working weather app

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +42,73 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (user_field.getText().toString().trim().isEmpty()){
                     Toast.makeText(MainActivity.this, R.string.warning, Toast.LENGTH_LONG).show();
+                }else{
+                    String city = user_field.getText().toString();
+                    String key = "03db4e687d3b38ada9e4023c4596fa7b";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q=" +  city + "&appid=" + key + "&units=metric";
+
+                    new GetURLData().execute(url);
                 }
             }
         });
+    }
+
+    private class GetURLData extends AsyncTask<String, String, String>{
+
+        protected void onPreExecute(){
+            super.onPreExecute();
+            result_info.setText("Loading....");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line = "";
+                StringBuffer buffer = new StringBuffer();
+
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line).append("\n");
+
+                    return buffer.toString();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }return null;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            JSONObject object = null;
+            try {
+                object = new JSONObject(result);
+                result_info.setText("Temperature: " + object.getJSONObject("main").getDouble("temp"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
