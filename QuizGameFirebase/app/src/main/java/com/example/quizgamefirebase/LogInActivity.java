7544b913ad5fcca0f1bfff23fdb2e294fd60c130
viking736 <viking25082000@gmail.com>
2021-07-16 13:26:01 +0300

    creating SignUpActivity

--- QuizGameFirebase/app/src/main/java/com/example/quizgamefirebase/LogInActivity.java
+++ QuizGameFirebase/app/src/main/java/com/example/quizgamefirebase/LogInActivity.java
@@ -2,13 +2,48 @@ package com.example.quizgamefirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.common.SignInButton;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonSignIn;
+    private TextView textViewSignUp, textViewForgetPassword;
+    private SignInButton signInButtonGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+        signInButtonGoogle = (com.google.android.gms.common.SignInButton) findViewById(R.id.signInButtonGoogle);
+        textViewSignUp = findViewById(R.id.textViewSignUp);
+        textViewForgetPassword = findViewById(R.id.textViewForgetPassword);
+
+        buttonSignIn.setOnClickListener(view->{
+
+        });
+
+        signInButtonGoogle.setOnClickListener(view->{
+
+        });
+
+        textViewSignUp.setOnClickListener(view->{
+            Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+        textViewForgetPassword.setOnClickListener(view->{
+
+        });
     }
 }
---
+++ QuizGameFirebase/app/src/main/java/com/example/quizgamefirebase/SignUpActivity.java
@@ -0,0 +1,57 @@
+package com.example.quizgamefirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editTextUserEmail, editTextUserPassword;
+    private Button buttonUserSignUp;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        editTextUserEmail = (EditText) findViewById(R.id.editTextUserEmail);
+        editTextUserPassword = (EditText) findViewById(R.id.editTextUserPassword);
+        buttonUserSignUp = (Button) findViewById(R.id.buttonUserSignUp);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        buttonUserSignUp.setOnClickListener(view->{
+
+            buttonUserSignUp.setClickable(false);
+            
+            String userEmail = editTextUserEmail.getText().toString();
+            String userPassword = editTextUserPassword.getText().toString();
+
+            signUpFirebase(userEmail, userPassword);
+        });
+    }
+
+    public void signUpFirebase(String userEmail, String userPassword){
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        auth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(SignUpActivity.this, "Your account is created!", Toast.LENGTH_SHORT).show();
+                finish();
+                progressBar.setVisibility(View.INVISIBLE);
+            }else{
+                Toast.makeText(SignUpActivity.this, "An error has occurred :( \n Please try later", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
